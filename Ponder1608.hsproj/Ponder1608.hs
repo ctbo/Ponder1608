28e365f11750b40f982677553847cdae89e521a7
Harald Bögeholz <bo@ct.de>
2016-09-04 14:28:45 +0200

    changed main to allow experimenting with n.

--- Ponder1608.hsproj/Ponder1608.hs
+++ Ponder1608.hsproj/Ponder1608.hs
@@ -7,27 +7,28 @@ import Data.IntSet (IntSet)
 import qualified Data.IntSet as S
 import Data.List
 import Control.Monad
-import Data.Maybe
+import System.Environment
 
 type State = ([Int],IntSet)
 
 initialState :: State
 initialState = ([], S.empty)
 
-n = 174
-
 pick0to2 :: [a] -> [[a]]
 pick0to2 = filter (\xs -> length xs <= 2) . subsequences
 
-addNumber :: State -> [State]
-addNumber (xs, forbidden) = concatMap f [start, start-1 .. 1]
+addNumber :: Int -> State -> [State]
+addNumber n (xs, forbidden) = concatMap f [start, start-1 .. 1]
   where start = if null xs then n else head xs - 1
         f x = if S.null $ S.intersection forbidden newSums
                 then [(x:xs, S.union forbidden newSums)]
                 else []
           where newSums = S.fromList $ map (sum . (x:)) $ pick0to2 xs
  
-solutions = (iterate (>>= addNumber) [initialState]) !! 10
+solutions n = (iterate (>>= addNumber n) [initialState]) !! 10
 
-main = sequence_ $ map (print.fst) solutions
+main = do
+    args <- getArgs
+    let n = case args of [] -> 174; [s] -> read s
+    print (fst (head (solutions n)))
 
